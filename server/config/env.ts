052c7a53ff28b685f078dd46970579a1c314e827
k-u-kiran01 <udaykiran00701@gmail.com>
2025-06-18 11:26:43 +0530

    Update env.ts

--- server/config/env.ts
+++ server/config/env.ts
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 
-config({path : `.env.${process.env.NODE_ENV || "development"}.local`})
+config();
 
-export const {port , NODE_ENV, db_url , jwt_secret_key , jwt_expiry} = process.env
+export const {port="5000" , NODE_ENV, db_url , jwt_secret_key , jwt_expiry} = process.env
