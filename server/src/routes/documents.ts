c5f4196e75a9e123dd68596830f94a3e3a297088
k-u-kiran01 <udaykiran00701@gmail.com>
2025-06-19 18:30:32 +0530

    Update documents.ts

--- server/src/routes/documents.ts
+++ server/src/routes/documents.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { addCollaborators, listcollaborators, removecollaborator, viewDoc } from "../controllers/documents";
 import {exportDoc} from '../controllers/exportController'
 import authorise from "../middlewares/auth";
+import { asyncHandler } from "../utils/asyncHandler";
 const docsRoute =Router();
 // /api/docs/
 docsRoute.get('/md/:id', viewDoc)
 
 docsRoute.get('/md/:id/contributors', listcollaborators)
 
-docsRoute.post('/md/:id/contributors', authorise,addCollaborators)
+docsRoute.post('/md/:id/contributors', authorise,asyncHandler(addCollaborators))
 
 docsRoute.delete('/md/:id/contributors', authorise,removecollaborator)
 
